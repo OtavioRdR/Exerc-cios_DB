d1b14ae2a464399cadd136d93845e719f0e92729
OtavioRdR <otaviorosa.rdr@gmail.com>
2024-11-12 15:21:18 -0300

    Comitando exercicios primeiro e segundo exercicio de 20

---
+++ src/Idade.java
@@ -0,0 +1,14 @@
+public class Idade {
+    public static void main(String[] args) {
+
+        int idade = 17;
+
+        if (idade < 18){
+            System.out.println("Menor de Idade");
+        }
+
+        else {
+            System.out.println("maior de idade");
+        }
+    }
+}
---
+++ src/ProdutosComDesconto.java
@@ -0,0 +1,26 @@
+public class ProdutosComDesconto {
+    public static void main(String[] args) {
+
+        int unidadeLaranja = 55;
+        int valorLaranja = 20;
+
+        if (unidadeLaranja >= 50) {
+            double desconDeCinquenta = valorLaranja * 0.50;
+            double valorDescontoDeCinquenta = valorLaranja - desconDeCinquenta;
+            System.out.println("O valor com desconto de 50% pela quantidade foi de: " + valorDescontoDeCinquenta);
+
+        } else if (unidadeLaranja >= 21) {
+            double descontoDeVinte = valorLaranja * 0.20;
+            double valorComDescontoDeVinte = valorLaranja - descontoDeVinte;
+            System.out.println("O valor com desconto de 20% pela quantidade foi de: " + valorComDescontoDeVinte);
+
+        } else if (unidadeLaranja >= 11) {
+            double desconto = valorLaranja * 0.10;
+            double valorComDesconto = valorLaranja - desconto;
+            System.out.println("O valor com desconto de 10% pela quantidade foi de: " + valorComDesconto);
+
+        } else {
+            System.out.println("Preço de " + unidadeLaranja + " unidades totais: " + unidadeLaranja);
+        }
+    }
+}
