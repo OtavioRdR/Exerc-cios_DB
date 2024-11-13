4fa02b2eb8cf9083d460308ffd8700a17bb0a84b
OtavioRdR <otaviorosa.rdr@gmail.com>
2024-11-13 17:40:46 -0300

    exercício de numeros aleatorio

---
+++ src/NumerosAleatorios.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+
+        Random aleatorio = new Random();
+        int[] numeros = new int[10];
+
+        System.out.println("gerenado 10 numeros aleatorios de 0 a 100: ");
+
+
+        numeros[0] = aleatorio.nextInt(101);
+        int menorNumero = numeros[0];
+        int maiorValor = numeros[0];
+
+
+        for (int i = 1; i < 10; i++){
+            numeros[i] = aleatorio.nextInt(101);
+
+            if (numeros[i] > maiorValor){
+
+                maiorValor = numeros[i];
+            }
+
+            if (numeros[i] < menorNumero){
+
+                menorNumero = numeros[i];
+
+            }
+        }
+
+        System.out.print("numeros: ");
+
+        for (int i = 0; i < 10; i++){
+
+            System.out.print(numeros[i]);
+
+            if (i < 9) {
+
+                System.out.print(", ");
+
+            }
+        }
+
+        System.out.println("\nmaior: " + maiorValor + " | menor: " + menorNumero);
+
+        }
+    }
+    //a mais dificil até agora, mais de 1 hora nesse codigo...
+
