0203db798601d44e531bac7fbf8c12b9fcd2b267
OtavioRdR <otaviorosa.rdr@gmail.com>
2024-11-25 14:11:05 -0300

    alterações na Main

--- src/OrientacaoAObjetos/CarroEx/Main.java
+++ src/OrientacaoAObjetos/CarroEx/Main.java
@@ -3,12 +3,12 @@ package OrientacaoAObjetos.CarroEx;
 public class Main {
     public static void main(String[] args) {
         CarroAutomatico meuCarro = new CarroAutomatico(180);
-        meuCarro.ligar(false); // Não liga
-        meuCarro.ligar(true); // Liga
+        meuCarro.ligar(false);
+        meuCarro.ligar(true);
 
         MotoPartidaPedal minhaMoto = new MotoPartidaPedal(150);
-        minhaMoto.ligar(false); // Não liga
-        minhaMoto.ligar(true); // Liga
+        minhaMoto.ligar(false);
+        minhaMoto.ligar(true);
 
         Guincho meuGuincho = new Guincho(120);
         meuGuincho.carregar(meuCarro);
